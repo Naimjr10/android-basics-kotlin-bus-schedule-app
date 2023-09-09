34f11b5c4a28f2b797ffaf466b603197859414ef
Naimjr10 <njr848262@gmail.com>
2023-09-09 16:21:46 +0700

    Create live respond to data changes in database using Flow

--- app/src/main/java/com/example/busschedule/database/schedule/ScheduleDao.kt
+++ app/src/main/java/com/example/busschedule/database/schedule/ScheduleDao.kt
@@ -2,6 +2,7 @@ package com.example.busschedule.database.schedule
 
 import androidx.room.Dao
 import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 
 /*
  * DAO stands for Data Access Object and is a Kotlin class that provides access to the data.
@@ -13,10 +14,10 @@ import androidx.room.Query
 interface ScheduleDao {
 
     @Query("SELECT * FROM schedule ORDER BY arrival_time ASC")
-    fun getAll(): List<Schedule>
+    fun getAll(): Flow< List<Schedule> >
 
     // You can reference Kotlin values from the query by preceding it with a colon (:)
     // (e.g. :stopName from the function parameter).
     @Query("SELECT * FROM schedule WHERE stop_name = :stopName ORDER BY arrival_time ASC")
-    fun getByStopName(stopName: String): List<Schedule>
+    fun getByStopName(stopName: String): Flow<List<Schedule>>
 }
